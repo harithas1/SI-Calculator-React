3cdfb0b77ab8fab4411355bb31ddbdf0f6cdb200
harithas1 <harithaharitha0862s@gmail.com>
2024-10-15 18:16:49 +0530

    SIC initial commit

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const __dirname = dirname(__filename);
 
 
 export default defineConfig({
-  base: "/simple-interest-calculator/",
+  base: "/SI-Calculator-React/",
   plugins: [react()],
   resolve: {
     alias: {
