92d17ed99d5be763923297b7efb1b39496a3244f
harithas1 <harithaharitha0862s@gmail.com>
2024-10-15 17:44:37 +0530

    SIC inital commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 export default defineConfig({
   plugins: [react()],
